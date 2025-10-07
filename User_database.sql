e57aad4825a2338e8400dc901976f4fb7eaf8869
Nvkiero <154574359+Nvkiero@users.noreply.github.com>
2025-10-07 20:56:32 +0700

    Add files via upload

---
+++ User_database.sql
@@ -0,0 +1,19 @@
+﻿
+INSERT INTO khachhang (
+    username,
+    matKhau,
+    hoTen,
+    ngaySinh,
+    email,
+    gioiTinh,
+    soDienThoai
+)
+VALUES (
+    N'admin01',
+    N'123456',
+    N'Nguyễn Văn A',
+    '1995-05-20',
+    N'nguyenvana@example.com',
+    N'Nam',
+    N'0912345678'
+);
